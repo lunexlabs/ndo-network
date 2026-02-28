c5ff8f8be0908c4253dcd9520c017dabba9e7871
lunexlabs <lunexlabs@proton.me>
2026-02-28 11:46:55 -0600

    fix

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,6 @@
 import Container from "../../src/components/layout/Container";
 import Image from "next/image";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "About NDO Network",
